aeee31ad2dfe6d3555392a01dc63b695dc13bf00
dnowak <dawid.nowak886@gmail.com>
2016-08-02 20:08:02 +0200

    added GameScreen changed SnakeGame to extend Game

---
+++ core/src/it/works/snake/GameScreen.java
@@ -0,0 +1,28 @@
+package it.works.snake;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen extends ScreenAdapter {
+
+    SpriteBatch batch;
+    Texture img;
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+}
